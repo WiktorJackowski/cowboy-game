82183bcd01365e2c7256fb12a2e4737c72f95d1a
WiktorJackowski <wjjacki@o2.pl>
2019-12-17 19:36:24 +0100

    Configurations physics of sprites

--- game.js
+++ game.js
@@ -9,56 +9,19 @@ function preload() {
 }
 
 function create() {
-    game.add.tileSprite(0,0,1000,800,'background');
-    ball = game.add.sprite(200,0, 'pilka');
-    player = game.add.sprite(0,600,'kowboj');
+    prepareSprites();
 
     game.physics.arcade.enable([player, ball]) ;
     game.physics.arcade.gravity.y = 200;
-    //player.body.bounce.y = 0.95;
+
     player.body.collideWorldBounds = true;
     player.body.allowGravity = false;
     player.body.immovable = true;
 
-
-
     ball.body.bounce.y = 0.95;
+    ball.body.bounce.x = 0.95;
     ball.body.collideWorldBounds = true;
-
-    // game.stage.backgroundColor = '#124184';
-    //
-    // sprite = game.add.sprite(300, 0, 'gameboy', 2);
-    // sprite2 = game.add.sprite(300, 400, 'gameboy', 3);
-    //
-    // game.physics.arcade.enable([sprite, sprite2]);
-    //
-    // game.physics.arcade.gravity.y = 200;
-    //
-    // sprite.body.bounce.y = 0.95;
-    // sprite.body.collideWorldBounds = true;
-    //
-    // sprite2.body.allowGravity = false;
-    // sprite2.body.immovable = true;
-    //
-    // game.input.onDown.add(toggleBody, this)
-
-}
-
-function toggleBody() {
-
-    //	Here we simply disable the body entirely
-    //	This stops all motion and collision checks against it
-    //	without actually destroying the body object itself.
-
-    if (sprite2.body.enable)
-    {
-        sprite2.body.enable = false;
-    }
-    else
-    {
-        sprite2.body.enable = true;
-    }
-
+    ball.body.velocity.setTo(200,200);
 }
 
 function update() {
@@ -67,4 +30,11 @@ function update() {
 
 function render() {
 
+}
+
+function prepareSprites(){
+    game.add.tileSprite(0,0,1000,800,'background');
+    ball = game.add.sprite(800,0, 'pilka');
+    player = game.add.sprite(0,600,'kowboj');
+    player.scale.setTo(0.8,0.8);
 }
